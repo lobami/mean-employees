b764f01f05219a765342d0cce5f6058b4c9b7705
Loth Mejia Martinez <eljugador360@gmail.com>
2018-10-14 15:56:06 -0500

    backend terminado (basico)

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -14,13 +14,26 @@ EmployeeCtrl.createEmployee = async (req, res) =>{
     
 
 ;}
-EmployeeCtrl.getEmployee = function(){
+EmployeeCtrl.getEmployee = async (req, res) => {
+    
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
+};
+EmployeeCtrl.editEmployee = async (req, res) => {
+    const { id } = req.params;
+    const employee = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+    await Employee.findByIdAndUpdate(id, {$set: employee}, {new: true});
+    res.json("empleado actualizado");
 
 };
-EmployeeCtrl.editEmployee = function(){
-
-};
-EmployeeCtrl.delateEmployee = function(){
+EmployeeCtrl.delateEmployee = async (req, res) => {
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json({status: 'empleado eliminado'});
     
 };
 module.exports = EmployeeCtrl;
